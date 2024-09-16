de0c0824e878679a1446f7844a46e79b3a7d6f06
Aleksei Blinov <alvablinov@gmail.com>
2024-09-16 10:30:16 +0300

    add external database variables

--- keycloak.tf
+++ keycloak.tf
@@ -1,21 +1,45 @@
-resource "kubernetes_namespace" "keycloak-namespace" {
+resource "kubernetes_namespace" "keycloak_namespace" {
   metadata {
-    name = var.keycloak-namespace
+    name = var.keycloak_namespace
   }
 }
 
+locals {
+  use_custom_values = var.helm_custom_values ? templatefile(var.helm_custom_values_path, {
+    admin_username                  = var.admin_username,
+    admin_password                  = var.admin_password,
+    proxy_type                      = var.proxy_type,
+    configmap_name                  = var.configmap_name,
+    default_postgresql              = var.default_postgresql,
+    host                            = var.db_credentials["host"],
+    port                            = var.db_credentials["port"],
+    database                        = var.db_credentials["dbname"],
+    user                            = var.db_credentials["username"],
+    password                        = var.db_credentials["password"],
+    db_credentials_secret           = var.db_credentials_secret,
+    existing_secret                 = var.db_credentials_secret["secret_name"],
+    existing_secret_host_key        = var.db_credentials_secret["host_key"],
+    existing_secret_port_key        = var.db_credentials_secret["port_key"],
+    existing_secret_user_key        = var.db_credentials_secret["username_key"],
+    existing_secret_database_key    = var.db_credentials_secret["dbname_key"],
+    existing_secret_password_key    = var.db_credentials_secret["password_key"],
+    production_mode                 = var.production_mode,
+    custom_certificates_secret_name = var.custom_certificates_secret ? var.custom_certificates_secret_name : ""
+  }) : ""
+}
+
 resource "argocd_application" "keycloak" {
-  count = lower(var.deploy-method) == "argocd" ? 1 : 0
+  count = lower(var.deploy_method) == "argocd" ? 1 : 0
 
   metadata {
-    name      = var.keycloak-name
-    namespace = var.argocd-namespace
+    name      = var.keycloak_name
+    namespace = var.argocd_namespace
   }
 
   spec {
     destination {
-      server    = var.cluster-url
-      namespace = kubernetes_namespace.keycloak-namespace.metadata[0].name
+      server    = var.cluster_url
+      namespace = kubernetes_namespace.keycloak_namespace.metadata[0].name
     }
 
     sync_policy {
@@ -23,105 +47,89 @@ resource "argocd_application" "keycloak" {
     }
 
     source {
-      repo_url        = var.helm-chart-repo
-      chart           = var.helm-chart-name
-      target_revision = var.helm-chart-version
+      repo_url        = var.helm_chart_repo
+      chart           = var.helm_chart_name
+      target_revision = var.helm_chart_version
 
       helm {
-        values = var.helm-custom-values ? templatefile(var.helm-custom-values-path, {
-          admin-username                  = var.admin-username,
-          admin-password                  = var.admin-password,
-          proxy-type                      = var.proxy-type,
-          configmap-name                  = var.configmap-name,
-          default-postgresql              = var.default-postgresql,
-          custom-certificates-secret-name = var.custom-certificates-secret ? var.custom-certificates-secret-name : ""
-        }) : ""
+        values = local.use_custom_values
       }
     }
   }
 }
 
 resource "helm_release" "keycloak" {
-  count = lower(var.deploy-method) == "helm" ? 1 : 0
+  count = lower(var.deploy_method) == "helm" ? 1 : 0
 
-  namespace  = kubernetes_namespace.keycloak-namespace.metadata[0].name
-  name       = var.helm-name
-  chart      = var.helm-chart-name
-  repository = var.helm-chart-repo
-  version    = var.helm-chart-version
+  namespace  = kubernetes_namespace.keycloak_namespace.metadata[0].name
+  name       = var.helm_name
+  chart      = var.helm_chart_name
+  repository = var.helm_chart_repo
+  version    = var.helm_chart_version
 
-  values = var.helm-custom-values ? [
-    templatefile(var.helm-custom-values-path, {
-      admin-username                  = var.admin-username,
-      admin-password                  = var.admin-password,
-      proxy-type                      = var.proxy-type,
-      configmap-name                  = var.configmap-name,
-      default-postgresql              = var.default-postgresql,
-      custom-certificates-secret-name = var.custom-certificates-secret ? var.custom-certificates-secret-name : ""
-    })
-  ] : []
+  values = var.helm_custom_values ? [local.use_custom_values] : []
 }
 
-resource "kubernetes_manifest" "custom-certificates-secret" {
-  count = var.custom-certificates-secret == true ? 1 : 0
+resource "kubernetes_manifest" "custom_certificates_secret" {
+  count = var.custom_certificates_secret == true ? 1 : 0
 
   manifest = {
     "apiVersion" = "v1"
     "kind"       = "Secret"
     "data" = {
-      "${var.custom-certificates-secret-name}.pem" = var.custom-certificates
+      "${var.custom_certificates_secret_name}.pem" = var.custom_certificates
     }
     "metadata" = {
-      "name" : var.custom-certificates-secret-name
-      "namespace" : kubernetes_namespace.keycloak-namespace.metadata[0].name
+      "name" : var.custom_certificates_secret_name
+      "namespace" : kubernetes_namespace.keycloak_namespace.metadata[0].name
     }
   }
 }
 
-resource "kubernetes_manifest" "keycloak-cloudnativepg-database-cluster" {
-  count = var.cloudnativepg-database == true ? 1 : 0
+resource "kubernetes_manifest" "keycloak_cloudnativepg_database_cluster" {
+  count = var.cloudnativepg_database == true ? 1 : 0
 
   manifest = {
     "apiVersion" = "postgresql.cnpg.io/v1"
     "kind"       = "Cluster"
     "metadata" = {
-      "name"      = "${var.helm-chart-name}-pg-cluster"
-      "namespace" = kubernetes_namespace.keycloak-namespace.metadata[0].name
+      "name"      = "${var.helm_chart_name}-pg-cluster"
+      "namespace" = kubernetes_namespace.keycloak_namespace.metadata[0].name
     }
     "spec" = {
       "bootstrap" = {
         "initdb" = {
-          "database" = var.helm-chart-name
-          "owner"    = var.helm-chart-name
+          "database" = var.helm_chart_name
+          "owner"    = var.helm_chart_name
           "secret" = {
-            "name" = "${var.helm-chart-name}-pg-secret"
+            "name" = "${var.helm_chart_name}-pg-secret"
           }
         }
       }
-      "instances" = var.cloudnativepg-instances-replicas
+      "instances" = var.cloudnativepg_instances_replicas
       "storage" = {
-        "size" = var.cloudnativepg-storage-size
+        "size" = var.cloudnativepg_storage_size
       }
     }
   }
 }
 
-resource "kubernetes_manifest" "keycloak-cloudnativepg-database-secret" {
-  count = var.cloudnativepg-database == true ? 1 : 0
+resource "kubernetes_manifest" "keycloak_cloudnativepg_database_secret" {
+  count = var.cloudnativepg_database == true ? 1 : 0
 
   manifest = {
     "apiVersion" = "v1"
     "data" = {
-      "db-host"  = base64encode("${var.helm-chart-name}-pg-cluster-rw.${kubernetes_namespace.keycloak-namespace.metadata[0].name}.svc.cluster.local")
-      "db-port"  = base64encode(var.db-port)
-      "db-name"  = base64encode(var.helm-chart-name)
-      "username" = base64encode(var.helm-chart-name)
-      "password" = base64encode(var.db-password)
+      "db-host"  = base64encode("${var.helm_chart_name}-pg-cluster-rw.${kubernetes_namespace.keycloak_namespace.metadata[0].name}.svc.cluster.local")
+      "db-port"  = base64encode(var.cloudnativepg_database_port)
+      "db-name"  = base64encode(var.helm_chart_name)
+      "username" = base64encode(var.helm_chart_name)
+      "password" = base64encode(var.cloudnativepg_database_password)
     }
     "kind" = "Secret"
     "metadata" = {
-      "name"      = "${var.helm-chart-name}-pg-secret"
-      "namespace" = kubernetes_namespace.keycloak-namespace.metadata[0].name
+      "name"      = "${var.helm_chart_name}-pg-secret"
+      "namespace" = kubernetes_namespace.keycloak_namespace.metadata[0].name
     }
     "type" = "Opaque"
   }
--- variables.tf
+++ variables.tf
@@ -1,205 +1,259 @@
-variable "kubeconfig-path" {
+variable "kubeconfig_path" {
   description = "Kubeconfig Path"
   type        = string
   default     = "~/.kube/config"
 }
 
-variable "kube-context" {
+variable "kube_context" {
   description = "Kubernetes Context to Use"
   type        = string
   default     = ""
 }
 
-variable "deploy-method" {
+variable "deploy_method" {
   description = "Choose Deploy Method ArgpCD or Helm"
   type        = string
 }
 
-variable "argocd-server" {
+variable "argocd_server" {
   description = "ArgoCD Server URL"
   type        = string
   default     = ""
 
   validation {
-    condition     = !(lower(var.deploy-method) == "argocd" && var.argocd-server == "")
+    condition     = !(lower(var.deploy_method) == "argocd" && var.argocd_server == "")
     error_message = "ArgoCD Server URL Must Not be NULL When Deploy Method is ArgoCD."
   }
 }
 
-variable "argocd-token" {
+variable "argocd_token" {
   description = "ArgoCD Token"
   type        = string
   sensitive   = true
   default     = ""
 
   validation {
-    condition     = !(lower(var.deploy-method) == "argocd" && var.argocd-token == "")
+    condition     = !(lower(var.deploy_method) == "argocd" && var.argocd_token == "")
     error_message = "ArgoCD Token Must Not be NULL When Deploy Method is ArgoCD."
   }
 }
 
-variable "tls-verify-skip" {
+variable "tls_verify_skip" {
   description = "Skip SelfSigned Certificates Validate"
   type        = bool
   default     = false
 }
 
-variable "keycloak-namespace" {
+variable "keycloak_namespace" {
   description = "Keycloak Namespace"
   type        = string
   default     = "keycloak"
 }
 
-variable "keycloak-name" {
+variable "keycloak_name" {
   description = "Keycloak Name"
   type        = string
   default     = "keycloak"
 }
 
-variable "argocd-namespace" {
+variable "argocd_namespace" {
   description = "ArgoCD Namespace"
   type        = string
   default     = "argocd"
 }
 
-variable "cluster-url" {
+variable "cluster_url" {
   description = "Cluster URL"
   type        = string
   default     = "https://kubernetes.default.svc"
 }
 
-variable "helm-name" {
+variable "helm_name" {
   description = "Helm Release Name"
   type        = string
   default     = "keycloak"
 }
 
-variable "helm-chart-name" {
+variable "helm_chart_name" {
   description = "Helm Chart Name"
   type        = string
   default     = "keycloak"
 }
 
-variable "helm-chart-repo" {
+variable "helm_chart_repo" {
   description = "Helm Chart Repo"
   type        = string
   default     = "oci://registry-1.docker.io/bitnamicharts/"
 }
 
-variable "helm-chart-version" {
+variable "helm_chart_version" {
   description = "Helm Chart Version"
   type        = string
   default     = "22.1.0"
 }
 
-variable "helm-custom-values" {
+variable "helm_custom_values" {
   description = "Use Helm Custom Values"
   type        = bool
   default     = false
 }
 
-variable "helm-custom-values-path" {
+variable "helm_custom_values_path" {
   description = "Helm Custom Values Path"
   type        = string
   default     = ""
 
   validation {
-    condition     = !(var.helm-custom-values && var.helm-custom-values-path == "")
-    error_message = "helm-custom-values-path must not be null when helm-custom-values is true."
+    condition     = !(var.helm_custom_values && var.helm_custom_values_path == "")
+    error_message = "helm_custom_values_path must not be null when helm_custom_values is true."
   }
 }
 
-variable "admin-username" {
+variable "admin_username" {
   description = "Admin Username"
   type        = string
   default     = "user"
 }
 
-variable "admin-password" {
+variable "admin_password" {
   description = "Admin Password"
   type        = string
   sensitive   = true
   default     = ""
 }
 
-variable "proxy-type" {
+variable "proxy_type" {
   description = "Proxy Type"
   type        = string
   default     = ""
 }
 
-variable "configmap-name" {
+variable "configmap_name" {
   description = "Config Map with CA Certificates Bundle Name"
   type        = string
   default     = ""
 }
 
-variable "custom-certificates-secret" {
+variable "custom_certificates_secret" {
   description = "Use Keycloak custom CA certificates Secret. The secret will be mounted as a directory and configured using KC_TRUSTSTORE_PATHS"
   type        = bool
   default     = false
 }
 
-variable "custom-certificates-secret-name" {
+variable "custom_certificates_secret_name" {
   description = "Custom Certificates Secret Name"
   type        = string
   default     = ""
 
   validation {
-    condition     = !(var.custom-certificates-secret && var.custom-certificates-secret-name == "")
-    error_message = "custom-certificates-secret-name must not be null when custom-certificates-secret is true."
+    condition     = !(var.custom_certificates_secret && var.custom_certificates_secret_name == "")
+    error_message = "custom_certificates_secret_name must not be null when custom_certificates_secret is true."
   }
 }
 
-variable "custom-certificates" {
+variable "custom_certificates" {
   description = "Custom Certificates in Base64"
   type        = string
   default     = ""
 
   validation {
-    condition     = !(var.custom-certificates-secret && var.custom-certificates == "")
-    error_message = "custom-certificates must not be null when custom-certificates-secret is true."
+    condition     = !(var.custom_certificates_secret && var.custom_certificates == "")
+    error_message = "custom_certificates must not be null when custom_certificates_secret is true."
   }
 }
 
-variable "default-postgresql" {
+variable "default_postgresql" {
   description = "Default PostgreSQL"
   type        = bool
   default     = true
 }
 
-variable "cloudnativepg-database" {
+variable "use_db_credentials" {
+  description = "Use database credentials"
+  type        = bool
+  default     = false
+}
+
+variable "db_credentials" {
+  description = "DB Credentials"
+  sensitive   = true
+  type        = map(string)
+  default = {
+    host     = ""
+    port     = ""
+    dbname   = ""
+    username = ""
+    password = ""
+  }
+
+  validation {
+    condition     = !(var.use_db_credentials && (var.db_credentials.host == "" || var.db_credentials.port == "" || var.db_credentials.dbname == "" || var.db_credentials.username == "" || var.db_credentials.password == ""))
+    error_message = "All database credentials fields must be populated when 'use_db_credentials' is true."
+  }
+}
+
+variable "use_db_credentials_secret" {
+  description = "Use existing secret resource containing the database credentials"
+  type        = bool
+  default     = false
+}
+
+variable "db_credentials_secret" {
+  description = "DB Credentials Secret"
+  #  sensitive   = true
+  type = map(string)
+  default = {
+    secret_name  = ""
+    host_key     = ""
+    port_key     = ""
+    dbname_key   = ""
+    username_key = ""
+    password_key = ""
+  }
+
+  validation {
+    condition     = !(var.use_db_credentials_secret && (var.db_credentials_secret.secret_name == "" || var.db_credentials_secret.host_key == "" || var.db_credentials_secret.port_key == "" || var.db_credentials_secret.dbname_key == "" || var.db_credentials_secret.username_key == "" || var.db_credentials_secret.password_key == ""))
+    error_message = "All database credentials secret fields must be populated when 'use_db_credentials_secret' is true."
+  }
+}
+
+variable "cloudnativepg_database" {
   description = "Use CloudNative PG Database as External Database"
   type        = bool
   default     = false
 }
 
-variable "cloudnativepg-instances-replicas" {
+variable "cloudnativepg_database_port" {
+  description = "CloudNative PG Database Port"
+  type        = number
+  default     = 5432
+}
+
+variable "cloudnativepg_database_password" {
+  description = "CloudNative PG Database Password"
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = !(var.cloudnativepg_database && var.cloudnativepg_database_password == "")
+    error_message = "cloudnativepg_database_password must not be null when cloudnativepg_database is true."
+  }
+}
+
+variable "cloudnativepg_instances_replicas" {
   description = "CloudNative PG Instances Replicas"
   type        = number
   default     = 1
 }
 
-variable "cloudnativepg-storage-size" {
+variable "cloudnativepg_storage_size" {
   description = "CloudNative PG Storage Size"
   type        = string
   default     = "10Gi"
 }
 
-variable "db-port" {
-  description = "DB Port"
-  type        = number
-  default     = "5432"
-}
-
-variable "db-password" {
-  description = "DB Password"
-  type        = string
-  sensitive   = true
-  default     = ""
-
-  validation {
-    condition     = !(var.cloudnativepg-database && var.db-password == "")
-    error_message = "db-password must not be null when cloudnativepg-database is true."
-  }
+variable "production_mode" {
+  description = "Run Keycloak in production mode"
+  type        = bool
+  default     = false
 }
